57c30f7e40fea0450649fdfdbae83b0b7a729da6
jylee <jyl9311@gmail.com>
2024-11-25 14:43:14 +0900

    BeanFactory 직접 생성

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -0,0 +1,42 @@
+package tobyspring.hellospring.payment;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static java.math.BigDecimal.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentServiceTest {
+
+    @Test
+    @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
+
+        // 원화환산금액의 유효시간 계산
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+
+    }
+
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+        Payment payment = paymentService.prepare(1L, "USD", TEN);
+
+        // 환율정보 가져온다
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+
+        // 원화환산금액 계산
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
+    }
+}
