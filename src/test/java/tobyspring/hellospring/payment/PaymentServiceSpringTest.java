671e909bb3bb1f6c5e40519a80ad0a21c3bc8ddf
jylee <jyl9311@gmail.com>
2024-11-25 14:45:34 +0900

    BeanFactory 주입

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceSpringTest.java
@@ -2,8 +2,12 @@ package tobyspring.hellospring.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tobyspring.hellospring.ObjectFactory;
 import tobyspring.hellospring.TestObjectFactory;
 
@@ -14,11 +18,15 @@ import static java.math.BigDecimal.TEN;
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
+
+    @Autowired
+    BeanFactory beanFactory;
 
     @Test
     void convertedAmount() throws IOException {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(1L, "USD", TEN);
