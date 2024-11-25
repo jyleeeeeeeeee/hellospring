75c513eb5d2c89f2803ed2f449269cc4efb02186
jylee <jyl9311@gmail.com>
2024-11-25 16:01:10 +0900

    Clock을 이용한 시간 테스트

---
+++ src/test/java/tobyspring/hellospring/learningtest/ClockTest.java
@@ -0,0 +1,41 @@
+package tobyspring.hellospring.learningtest;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClockTest {
+    // Clock을 이용해서 LocalDateTime.now?
+    @Test
+    void clock() throws InterruptedException {
+        Clock clock = Clock.systemDefaultZone();
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        TimeUnit.SECONDS.sleep(2);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        assertThat(dt2).isAfter(dt1);
+    }
+
+    // Clock을 Test에서 사용할 때 내가 원하는 시간을 지정해서 현재 시간을 가져오게 할 수 있는가?
+    @Test
+    void fixedClock() {
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        LocalDateTime dt3 = LocalDateTime.now(clock).plusHours(1);
+
+        assertThat(dt1).isEqualTo(dt2);
+        assertThat(dt3).isEqualTo(dt1.plusHours(1));
+
+    }
+}
+
