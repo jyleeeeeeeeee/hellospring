86e74c8e5e5d9279e0149932bfc898eaf17f3f6a
jylee <jyl9311@gmail.com>
2024-11-26 10:49:07 +0900

    도메인 오브젝트 테스트

--- src/main/java/tobyspring/hellospring/payment/Payment.java
+++ src/main/java/tobyspring/hellospring/payment/Payment.java
@@ -1,9 +1,39 @@
 package tobyspring.hellospring.payment;
 
 import java.math.BigDecimal;
+import java.time.Clock;
 import java.time.LocalDateTime;
 
 public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public static Payment createPrepared(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate,
+                                         LocalDateTime now) {
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = now.plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    public boolean isValid(Clock clock) {
+        return LocalDateTime.now(clock).isBefore(this.validUntil);
+    }
+
     public Long getOrderId() {
         return orderId;
     }
@@ -28,14 +58,6 @@ public class Payment {
         return validUntil;
     }
 
-    private Long orderId;
-    private String currency;
-    private BigDecimal foreignCurrencyAmount;
-    private BigDecimal exRate;
-    private BigDecimal convertedAmount;
-    private LocalDateTime validUntil;
-
-
     @Override
     public String toString() {
         return "Payment{" +
@@ -46,14 +68,5 @@ public class Payment {
                 ", convertedAmount=" + convertedAmount +
                 ", validUntil=" + validUntil +
                 '}';
-    }
-
-    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
-        this.orderId = orderId;
-        this.currency = currency;
-        this.foreignCurrencyAmount = foreignCurrencyAmount;
-        this.exRate = exRate;
-        this.convertedAmount = convertedAmount;
-        this.validUntil = validUntil;
     }
 }
