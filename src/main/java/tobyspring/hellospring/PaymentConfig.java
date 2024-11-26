10a2947b38906ff522783bad773f42da6db1a8f7
jylee <jyl9311@gmail.com>
2024-11-26 17:34:45 +0900

    스프링이 제공하는 템플릿(RestTemplate)

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -2,10 +2,12 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExRateExtractor;
 import tobyspring.hellospring.api.HttpClientApiExecutor;
 import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
@@ -17,18 +19,18 @@ import static java.math.BigDecimal.valueOf;
 @Configuration
 public class PaymentConfig {
     @Bean
-    public ApiTemplate apiTemplate() {
-        return new ApiTemplate(new SimpleApiExecutor(), new ErApiExRateExtractor());
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider(), clock());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider(apiTemplate());
-    }
-
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider(), clock());
+        return new RestTemplateExRateProvider(restTemplate());
     }
 
     @Bean
