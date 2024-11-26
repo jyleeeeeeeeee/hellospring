390c40afe4af212c8ba7d269a4fbbd35616a7666
jylee <jyl9311@gmail.com>
2024-11-26 18:09:05 +0900

    JPA를 이용한 Order 저장

---
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -0,0 +1,33 @@
+package tobyspring.hellospring;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.order.Order;
+
+import java.math.BigDecimal;
+
+public class DataClient {
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
+        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+
+        // em
+        EntityManager em = emf.createEntityManager();
+
+        // transaction
+        em.getTransaction().begin();
+        Order order = new Order("100", BigDecimal.TEN);
+        System.out.println("order = " + order);
+
+
+        // em.persist
+        em.persist(order);
+        System.out.println("order = " + order);
+
+        em.getTransaction().commit();
+        em.close();
+
+    }
+}
