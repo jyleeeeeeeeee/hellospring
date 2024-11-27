052c61ed135f3612b5d4e49941b93650b564d74b
jylee <jyl9311@gmail.com>
2024-11-27 15:05:44 +0900

    트랜잭션 서비스 추상화

--- src/main/java/tobyspring/hellospring/OrderConfig.java
+++ src/main/java/tobyspring/hellospring/OrderConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import tobyspring.hellospring.data.JpaOrderRepository;
 import tobyspring.hellospring.order.OrderRepository;
 import tobyspring.hellospring.order.OrderService;
@@ -12,7 +13,7 @@ import tobyspring.hellospring.order.OrderService;
 @Import(DataConfig.class)
 public class OrderConfig {
     @Bean
-    public OrderService orderService(JpaTransactionManager transactionManager) {
+    public OrderService orderService(PlatformTransactionManager transactionManager) {
         return new OrderService(orderRepository(), transactionManager);
     }
 
