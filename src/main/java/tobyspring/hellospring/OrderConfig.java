948f93a2c48a93ac4f67340d45205ce09c61fc36
jylee <jyl9311@gmail.com>
2024-11-28 10:57:49 +0900

    @Transactional과 AOP

--- src/main/java/tobyspring/hellospring/OrderConfig.java
+++ src/main/java/tobyspring/hellospring/OrderConfig.java
@@ -3,23 +3,22 @@ package tobyspring.hellospring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.hellospring.data.JdbcOrderRepository;
-import tobyspring.hellospring.data.JpaOrderRepository;
 import tobyspring.hellospring.order.OrderRepository;
 import tobyspring.hellospring.order.OrderService;
 import tobyspring.hellospring.order.OrderServiceImpl;
-import tobyspring.hellospring.order.OrderServiceTxProxy;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
     @Bean
     public OrderService orderService(PlatformTransactionManager transactionManager, OrderRepository orderRepository) {
-        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), transactionManager);
+        return new OrderServiceImpl(orderRepository);
     }
 
     @Bean
